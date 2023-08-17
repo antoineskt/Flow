09ff0fe1e4e0d85d2b54686e20a5695540a5d126
antoineskt <billet.antoine42@gmail.com>
2023-08-17 23:55:34 +0200

    order

--- mobile/screens/FirstPage.js
+++ mobile/screens/FirstPage.js
@@ -1,75 +1,57 @@
-import {
-  ImageBackground,
-  Image,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TextInput,
-  Platform,
-  Button
-} from "react-native";
+import { Image, View, Text, StyleSheet, TouchableOpacity } from "react-native"
 
-import React, { useCallback, useEffect, useState }  from "react";
-import { LinearGradient } from 'expo-linear-gradient';
-import logoblanc from '../assets/logoblanc.png';
+import React from "react"
+import { LinearGradient } from "expo-linear-gradient"
+import logoblanc from "../assets/logoblanc.png"
 
-import { useFonts, Roboto_900Black } from '@expo-google-fonts/roboto';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { useNavigation } from '@react-navigation/native';
-
-
-
-
-
+import { useFonts, Roboto_900Black } from "@expo-google-fonts/roboto"
+import { useNavigation } from "@react-navigation/native"
 
 const FirstPage = () => {
+  const [fontsLoaded] = useFonts({ Roboto_900Black })
+  const navigation = useNavigation()
 
-  const [fontsLoaded] = useFonts({ Roboto_900Black });
-  const navigation = useNavigation();
-
-  
   if (!fontsLoaded) {
-    return null;
+    return null
   }
-
 
   return (
     <View style={styles.corps}>
-      
-      <LinearGradient style={styles.container}  colors={['#FF3B01', '#FACA21']} >
-
-       
+      <LinearGradient style={styles.container} colors={["#FF3B01", "#FACA21"]}>
         <Image source={logoblanc} style={styles.logo} />
-        
-        <Text style= {{ fontFamily: 'Roboto_900Black', fontSize: 16, color: "white", textTransform: "uppercase", textAlign: "center"}}>
-        Atteinds {"\n"}
-        tes {"\n"}
-        objectifs 
+        <Text
+          style={{
+            fontFamily: "Roboto_900Black",
+            fontSize: 16,
+            color: "white",
+            textTransform: "uppercase",
+            textAlign: "center",
+          }}
+        >
+          Atteinds {"\n"}
+          tes {"\n"}
+          objectifs
         </Text>
-        
-      
       </LinearGradient>
-      
-      
+
       <View style={styles.footer}>
-        <TouchableOpacity onPress={() => navigation.navigate('Signup')} style={styles.button}>
-        <Text style={styles.buttontext}> S'inscrire </Text> 
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Signup")}
+          style={styles.button}
+        >
+          <Text style={styles.buttontext}> S'inscrire </Text>
         </TouchableOpacity>
 
-        
-
-        <TouchableOpacity onPress={() => navigation.navigate("Login")} style={styles.buttondeux}>
-        <Text style={styles.buttontextdeux}> Se connecter </Text> 
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Login")}
+          style={styles.buttondeux}
+        >
+          <Text style={styles.buttontextdeux}> Se connecter </Text>
         </TouchableOpacity>
-
       </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   corps: {
@@ -81,71 +63,58 @@ const styles = StyleSheet.create({
   container: {
     witdh: "100%",
     height: "90%",
-    justifyContent:  "center", 
-    alignItems: "center", 
-    
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   logo: {
-    width: "72%", 
-    height: "58%", 
-    
-    //311 355px
-  
-    
+    width: "72%",
+    height: "58%",
   },
 
   footer: {
     flex: 1,
     flexDirection: "row",
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "100%"
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
   },
 
   button: {
-    
     width: "42%",
     height: "64%",
-    
-    
     backgroundColor: "white",
     borderWidth: 2,
     borderRadius: 6,
     borderColor: "black",
     alignItems: "center",
     justifyContent: "center",
-    marginRight: "1%"
+    marginRight: "1%",
   },
 
   buttondeux: {
-    
     width: "42%",
     height: "64%",
-    
-    
     backgroundColor: "black",
     borderWidth: 2,
     borderRadius: 6,
     borderColor: "black",
     alignItems: "center",
     justifyContent: "center",
-    
-
   },
 
   buttontext: {
     fontFamily: "Roboto_900Black",
     textTransform: "uppercase",
-    letterSpacing: 0.04
+    letterSpacing: 0.04,
   },
 
   buttontextdeux: {
     fontFamily: "Roboto_900Black",
     textTransform: "uppercase",
     letterSpacing: 0.04,
-    color: "white"
-  }
-});
+    color: "white",
+  },
+})
 
-export default FirstPage;
+export default FirstPage
